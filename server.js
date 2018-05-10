deb47958e196bb762f1b7869737419b7d5ca5a88
Eliot Rodriguez <mr.ear84@gmail.com>
2018-05-10 17:04:50 -0400

    able to push info back to the server :-)

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+//const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
 
 const app = express();
 app.use(methodOverride('_method'));
+//app.use(bodyParser.urlencoded({extended:false}));
 
 var PORT = process.env.PORT || 3000;
 
 const products = [
-	{id: 5, name: 'foo'},
+	{id: 1, name: 'foo'},
 	{id: 2, name: 'bar'},
-	{id: 3, name: 'foobar'}
+	{id: 3, name: 'foobar'},
+	{id: 4, name: 'barfoo'}
 ];
 
 app.engine('handlebars', exphbs({
@@ -25,8 +27,8 @@ app.get('/', (req, res, next) => {
 	res.render("index", { products });
 });
 
-app.delete('/:id?', (req, res, next) => {
-	res.send('your id is: ' + req.body.id);
+app.put('/:id', (req, res, next) => {
+	res.send('your id is put::: ' + req.params.id);//req.params.id);
 });
 
 app.listen(PORT, ()=> console.log('This shit is bananas'));
